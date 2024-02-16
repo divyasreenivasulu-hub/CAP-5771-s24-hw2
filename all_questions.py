9441fcb858a68ed7d02fdc2b1546ef0c9a1aaa22
divyasreenivasulu-hub <84560548+divyasreenivasulu-hub@users.noreply.github.com>
2024-02-15 23:27:05 -0500

    Update all_questions.py

--- all_questions.py
+++ all_questions.py
@@ -26,51 +26,51 @@ def question1():
     level1["smoking"] = 1.0
     level1["smoking_info_gain"] = 0.2780
 
-    level1["cough"] = - 1.0
-    level1["cough_info_gain"] = 0.2364
+    level1["cough"] = -1.0
+    level1["cough_info_gain"] = 0.0348
 
-    level1["radon"] = - 1.0
-    level1["radon_info_gain"] = 0.0348
+    level1["radon"] = -1.0
+    level1["radon_info_gain"] = 0.2364
 
-    level1["weight_loss"] = - 1.0
-    level1["weight_loss_info_gain"] = 0.0290
+    level1["weight_loss"] = -1.0
+    level1["weight_loss_info_gain"] = 0.0291
 
-    level2_left["smoking"] = - 1.0
+    level2_left["smoking"] = -1.0
     level2_left["smoking_info_gain"] = 0.0
-    level2_right["smoking"] = - 1.0
+    level2_right["smoking"] = -1.0
     level2_right["smoking_info_gain"] = 0.0
 
-    level2_left["radon"] = - 1.0
-    level2_left["radon_info_gain"] = 0.07290
+    level2_left["radon"] = -1.0
+    level2_left["radon_info_gain"] = 0.0729
 
     level2_left["cough"] = 1.0
-    level2_left["cough_info_gain"] = 0.72192
+    level2_left["cough_info_gain"] = 0.7219
 
-    level2_left["weight_loss"] = - 1.0
-    level2_left["weight_loss_info_gain"] = 0.1709
+    level2_left["weight_loss"] = -1.0
+    level2_left["weight_loss_info_gain"] = 0.1710
 
     level2_right["radon"] = 1.0
     level2_right["radon_info_gain"] = 0.7219
 
-    level2_right["cough"] = - 1.0
-    level2_right["cough_info_gain"] = 0.32192
+    level2_right["cough"] = -1.0
+    level2_right["cough_info_gain"] = 0.3219
 
-    level2_right["weight_loss"] = - 1.0
-    level2_right["weight_loss_info_gain"] = 0.17095
+    level2_right["weight_loss"] = -1.0
+    level2_right["weight_loss_info_gain"] = 0.1711
 
     answer["level1"] = level1
     answer["level2_left"] = level2_left
     answer["level2_right"] = level2_right
 
-    # Fill up construct_tree`
+    # Fill up `construct_tree``
     # tree, training_error = construct_tree()
-    tree = u.BinaryTree("smoking")  # MUST STILL CREATE THE TREE ***
+    tree = u.BinaryTree("smoking")  # MUST STILL CREATE THE TREE *****
     A = tree.insert_left("cough")
     B = tree.insert_right("radon")
     A.insert_left("Yes")
     A.insert_right("No")
     B.insert_left("Yes")
-    B.insert_right("No")
+    B.insert_right("No")  # MUST STILL CREATE THE TREE *****
     answer["tree"] = tree  # use the Tree structure
     # answer["training_error"] = training_error
     answer["training_error"] = 0.0  
@@ -79,35 +79,37 @@ def question1():
 
 
 # ----------------------------------------------------------------------
+
+
 def question2():
     answer = {}
 
     # Answers are floats
-    answer["(a) entropy_entire_data"] = 1.0
-
+    answer["(a) entropy_entire_data"] = 1.4253
     # Infogain
-    answer["(b) x < 0.2"] = 0.46438561897747244
-    answer["(b) x < 0.7"] = 0.3602012209808308
-    answer["(b) y < 0.6"] = 0.44217935649972373
+    answer["(b) x < 0.2"] = 0.1774
+    answer["(b) x < 0.7"] = 0.355
+    answer["(b) y < 0.6"] = 0.347
 
     # choose one of 'x=0.2', 'x=0.7', or 'x=0.6'
-    answer["(c) attribute"] = "x = 0.7"
+    answer["(c) attribute"] = "x<=0.7"  
 
     # Use the Binary Tree structure to construct the tree
     # Answer is an instance of BinaryTree
-    tree = u.BinaryTree("y = 0.7")
-    tree.insert_left("x=0.7")
-    tree.left.insert_left("B")
-    tree.left.insert_right("y=0.3")
-    tree.left.right.insert_left("A")
-    tree.left.right.insert_right("C")
-
-    tree.insert_right("x=0.2")
-    tree.right.insert_left("y=0.8")
-    tree.right.insert_right("A")
-    tree.right.left.insert_left("C")
-    tree.right.left.insert_right("B")
-    tree.print_tree()
+    
+    tree = u.BinaryTree("x<=0.7")
+    A=tree.insert_left("y<=0.6")
+    A.insert_left("B")
+    C=A.insert_right("x<=0.2")
+    D=C.insert_left("y<=0.8")
+    C.insert_right("A")
+    D.insert_left("C")
+    D.insert_right("B")
+    B=tree.insert_right("y<=0.6")
+    E=B.insert_left("y<=0.3")
+    B.insert_right("A")
+    E.insert_left("A")
+    E.insert_right("C")
     answer["(d) full decision tree"] = tree
 
     return answer
@@ -126,13 +128,12 @@ def question3():
     answer["(b) Gini, ID"] = 0.0
     answer["(c) Gini, Gender"] = 0.48
     answer["(d) Gini, Car type"] = 0.1625
-    answer["(e) Gini, Shirt type"] = 0.491
+    answer["(e) Gini, Shirt type"] = 0.4914
 
     answer["(f) attr for splitting"] = "Car type"
 
     # Explanatory text string
-    answer["(f) explain choice"] = ""The attribute that is used for splitting after the root node is car type because it has the least Gini index."
-
+    answer["(f) explain choice"] = "Car type has the least gini value"
 
     return answer
 
@@ -153,45 +154,45 @@ def question4():
     #  'quantitative', 'interval', 'ratio'
     # If you have a choice between 'binary' and 'discrete', choose 'binary'
 
-    answer["a"] = ['binary', 'quantitative', 'interval']
+    answer["a"] = ['binary','qualitative','ordinal']
 
     # Explain if there is more than one interpretation. Repeat for the other questions. At least five words that form a sentence.
     answer["a: explain"] = ""
 
-    answer["b"] = ['continuous', 'quantitative', 'ratio']
+    answer["b"] = ['continuous','quantitative','ratio']
     answer["b: explain"] = ""
 
-    answer["c"] = [continuous', 'qualitative', 'ordinal']
+    answer["c"] = ['discrete','qualitative','ordinal']
     answer["c: explain"] = ""
 
-    answer["d"] = ['continuous', 'quantitative', 'ratio']
+    answer["d"] = ['continuous','quantitative','ratio']
     answer["d: explain"] = ""
 
-    answer["e"] = ['discrete', 'qualitative', 'ordinal']
+    answer["e"] = ['discrete','qualitative','ordinal']
     answer["e: explain"] = ""
 
-    answer["f"] = ['continuous', 'quantitative', 'ratio']
+    answer["f"] = ['continuous','quantitative','interval']
     answer["f: explain"] = ""
 
-    answer["g"] = ['discrete', 'quantitative', 'ratio']
+    answer["g"] = ['discrete','quantitative','ratio']
     answer["g: explain"] = ""
 
-    answer["h"] = ['discrete', 'qualitative', 'nominal']
+    answer["h"] = ['discrete','qualitative','nominal']
     answer["h: explain"] = ""
 
-    answer["i"] = ['discrete', 'qualitative', 'nominal']
+    answer["i"] = ['discrete','qualitative','ordinal']
     answer["i: explain"] = ""
 
-    answer["j"] = ['discrete', 'qualitative', 'ordinal']
+    answer["j"] = ['discrete','qualitative','ordinal']
     answer["j: explain"] = ""
 
-    answer["k"] = ['continuous', 'quantitative', 'ratio']
+    answer["k"] = ['continuous','quantitative','ratio']
     answer["k: explain"] = ""
 
-    answer["l"] = ['continuous', 'quantitative', 'ratio']
+    answer["l"] = ['continuous','quantitative','ratio']
     answer["l: explain"] = ""
 
-    answer["m"] = ['discrete', 'qualitative', 'nominal']
+    answer["m"] = ['discrete','qualitative','nominal']
     answer["m: explain"] = ""
 
     return answer
@@ -207,21 +208,21 @@ def question5():
 
     # string: one of 'Model 1' or 'Model 2'
     explain["a"] = "Model 2"
-    explain["a explain"] = "This is because model-2 is pruned version and it is less likely to overfit the data and have better performances on unseen data."
-
+    explain["a explain"] = "Model 1 has high accuracy on A but lower accuracy on B, indicating overfitting; whereas, Model 2's accuracy on both datasets A and B is nearly same, indicating superior generalization to new data. "
 
     # string: one of 'Model 1' or 'Model 2'
-    explain["b"] = "Model 1"
-    explain["b explain"] = "Even though Model 1 performed better on the combined dataset (A + B), Model 2 would be the better option for classification tasks because of its better generalization capabilities. Importantly for real-world applications, Model 2 is more likely to perform well on unknown situations and is less likely to overfit to the training data."
+    explain["b"] = "Model 2"
+    explain["b explain"] = "Despite a minor loss in accuracy, Model 2 is still superior due to improved generalization and a lower likelihood of overfitting."
 
+    explain["c similarity"] = "Penalty on Complexity / Regularization"
+    explain["c similarity explain"] = "They are similar, since they penalize the complexity of the model"
 
-    explain["c similarity"] = "Regularization"
-    explain["c similarity explain"] = "In order to aid in model selection and prevent overfitting, they both seek to incorporate model complexity into the loss function."
-
-    explain["c difference"] = ""Criterion"
-    explain["c difference explain"] = ""One difference between them is how they go about calculating model complexity, especially when it comes to decision trees."
+    explain["c difference"] = "Specificity"
+    explain["c difference explain"] = "MDL seeks a model with a low bit count for description, while pessimistic error tries to minimize tree error in order to prevent complicated decision trees."
 
     return explain
+
+
 # ----------------------------------------------------------------------
 def question6():
     answer = {}
@@ -231,45 +232,48 @@ def question6():
     # value of the form "z <= float" where "z" is "x" or "y"
     #  and "float" is a floating point number (notice: <=)
     # The value could also be "A" or "B" if it is a leaf
-    answer["a, level 1"] = "x <= 0.5"
+    answer["a, level 1"] = "x<=0.5"
     answer["a, level 2, right"] ="A"
-    answer["a, level 2, left"] = "y <= 0.4"
+    answer["a, level 2, left"] = "y<=0.4"
     answer["a, level 3, left"] = "A"
-    answer["a, level 3, right"] = "x <= 0.2"
+    answer["a, level 3, right"] = "B"
 
     # run each datum through the tree. Count the number of errors and divide by number of samples. .
     # Since we have areas: calculate the area that is misclassified (total area is unity)
     # float between 0 and 1
-    answer["b, expected error"] = 0.58
-
+    answer["b, expected error"] = 0.06
+    #Misclassification happens in the A where x<=0.2 and y>=0.7 and y<=1.0
+    #Thus the area is 0.06 which is the misclassification error
     # Use u.BinaryTree to define the tree. Create your tree.
     # Replace "root node" by the proper node of the form "z <= float"
-    tree = u.BinaryTree("x <= 0.5")
-
-    A = tree.insert_right("A")
-    B = tree.insert_left("y <= 0.4")
-    B.insert_left("A")
-    B.insert_right("x <= 0.2")
+    tree = u.BinaryTree("x<=0.5")
+    left = tree.insert_left("y<=0.4")
+    right = tree.insert_right("A")
+    left.insert_left("A")
+    left.insert_right("B")
     answer["c, tree"] = tree
+
     return answer
+
+
 # ----------------------------------------------------------------------
 def question7():
     answer = {}
 
     # float
-    answer["a, info gain, ID"] = 0.0
-    answer["b, info gain, Handedness"] = -1.474
+    answer["a, info gain, ID"] = 1.0
+    answer["b, info gain, Handedness"] = 0.531
 
     # string: "ID" or "Handedness"
-    answer["c, which attrib"] = "Based on the information gain, Handedness should not be chosen as the splitting attribute because it results in negative information gain. So we will choose the ID attribute"
+    answer["c, which attrib"] = "ID"
 
     # answer is a float
-    answer["d, gain ratio, ID"] = 0.0
-    answer["e, gain ratio, Handedness"] = -0.737
+    answer["d, gain ratio, ID"] = 0.2314
+    answer["e, gain ratio, Handedness"] = 0.531
 
     # string: one of 'ID' or 'Handedness' based on gain ratio
     # choose the attribute with the largest gain ratio
-    answer["f, which attrib"] = "Based"
+    answer["f, which attrib"] = "Handedness"
 
     return answer
 
@@ -285,6 +289,14 @@ if __name__ == "__main__":
     answers["q5"] = question5()
     answers["q6"] = question6()
     answers["q7"] = question7()
+
+    u.save_dict("answers.pkl", answers)
+
+
+
+   
+  
+
 
     u.save_dict("answers.pkl", answers)
 
